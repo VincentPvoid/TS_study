49596c296d766080e15970fa29e2c7b9cdb3bfd7
VincentPvoid <60776308+VincentPvoid@users.noreply.github.com>
2021-01-02 20:48:15 +0800

    Add files via upload

---
+++ TS_example/03_ts_webpack/src/index.ts
@@ -0,0 +1,16 @@
+function sum(a:number, b:number):number{
+  return a+b;
+}
+
+const obj = {name:'aaa', age:18}
+
+console.log(obj);
+
+obj.age = 11;
+
+console.log(obj);
+
+console.log('test');
+console.log(sum(123,456));
+
+console.log(Promise)
